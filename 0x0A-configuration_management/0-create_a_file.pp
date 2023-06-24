750f111119263fe2837758dadab0bde21cfc449b
amanabay <amanuelabay22@gmail.com>
2023-06-24 05:50:35 +0300

    Fix puppet lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 file { '/tmp/school':
-  ensure => present,
-  mode   => '0744',
-  owner  => 'www-data',
-  group  => 'www-data',
+  ensure  => present,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
 }
+
