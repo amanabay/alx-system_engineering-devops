68f230cea6c61bd571fee88dacff2168f39d439d
amanabay <amanuelabay22@gmail.com>
2023-07-23 04:09:26 +0300

    Puppetlint fix

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,8 +1,10 @@
 # Configure web_servers using puppet
 
 exec { 'command':
-  command => 'sudo apt-get update;
+  command  => 'sudo apt-get update;
   sudo apt-get -y install nginx;
-  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default; sudo service nginx -s reload',
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By
+  $HOSTNAME;" /etc/nginx/sites-available/default;
+  sudo service nginx -s reload',
   provider => shell,
 }
