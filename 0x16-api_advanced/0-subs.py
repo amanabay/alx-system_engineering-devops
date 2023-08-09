fc3efe429b72f89e05212f4b4a71c22fb14a34b1
amanabay <amanuelabay22@gmail.com>
2023-08-09 04:43:22 +0300

    Add allow_redirects parameter to response

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,7 +15,7 @@ def number_of_subscribers(subreddit):
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
-    response = requests.get(url)
+    response = requests.get(url, allow_redirects=False)
 
     if response.status_code == 200:
         try:
