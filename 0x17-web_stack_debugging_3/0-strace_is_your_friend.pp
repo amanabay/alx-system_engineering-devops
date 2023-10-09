a9bdb98f529d97bfa089c0e5045783c006a86e2c
amanabay <amanuelabay22@gmail.com>
2023-10-09 21:54:51 +0300

    Preparation for def

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,7 +0,0 @@
-# Fix wp-settings.php by replace wrong extension(phpp) by php
-
-exec { 'replace_phpp':
-  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-  path    => ['/usr/locale/bin/:/bin/']
-
-}
--- 0x1B-web_stack_debugging_4/README.md
+++
@@ -1,1 +0,0 @@
-# Web Stack Degugging 4
